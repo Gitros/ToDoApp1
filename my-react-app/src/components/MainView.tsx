ff737ec0fe26e0b65dcd975142e32b8c1e14a622
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2025-10-24 17:00:49 +0200

    css update

--- my-react-app/src/components/MainView.tsx
+++ my-react-app/src/components/MainView.tsx
@@ -3,14 +3,17 @@ import styles from "./MainView.module.css";
 const MainView = () => {
   return (
     <main className={styles.main}>
-      <h2 className={styles.task-Header}>Tasks</h2>
-      <ul>
+      <div className={styles["task-header"]}>
+        <h2 className={styles["task-text"]}>Tasks</h2>
+        <button className={styles["add-task-btn"]}>Add task</button>
+      </div>
+      <ul className={styles["task-list"]}>
         <li>Task 1</li>
         <li>Task 2</li>
         <li>Task 3</li>
       </ul>
     </main>
   );
-}
+};
 
 export default MainView;
--- my-react-app/src/components/Sidebar.tsx
+++ my-react-app/src/components/Sidebar.tsx
@@ -4,9 +4,10 @@ const Sidebar = () => {
   return (
     <div className={styles["side-bar"]}>
       <h1 className={styles.header}>To do App</h1>
-      <div>
-        <h2>Projects</h2>
-        <div>
+      <hr />
+      <div className={styles.projects}>
+        <h2 className={styles["projects-header"]}>Projects</h2>
+        <div className={styles["project-container"]}>
           <p>Project 1</p>
           <p>Project 2</p>
           <p>Project 3</p>
